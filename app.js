2033ca30dc9e7465f35b7932870ae44f9fd1d55b
cantrek2 <cantrek2@wwu.edu>
2021-11-17 02:05:48 -0800

    moved pie chart scripts to seperate JS file, merged survey

--- app.js
+++ app.js
@@ -1,25 +1,106 @@
-anychart.onDocumentReady(function() {
+$(function () {
+    const colorOne= '#3c8dbc';
+    const colorTwo= '#f56954';
+    const colorThree= '#f39c12';
+    const colorFour= 'blue';
+    const colorFive= 'yellow';
+    const colorSix= 'pink';
+    const choice= 'green';
+    var ctx = document.getElementById("Age_range").getContext('2d');
+    var data = {
+        datasets: [{
+            data: [10, 20, 30, 40, 50, 60],
+            backgroundColor: [
+                '#3c8dbc',
+                '#f56954',
+                '#f39c12',
+                choice
+                
+            ],
+        }],
+        labels: [
+            'Under 17',
+            '18-24',
+            '25-34',
+            '35-44',
+            '45-54',
+            '55-64',
+            '65 and over'
+        ]
+    };
+    var myDoughnutChart = new Chart(ctx, {
+        type: 'pie',
+        data: data,
+        options: {
+            responsive: false,
+            maintainAspectRatio: false,
+            legend: {
+                position: 'bottom',
+                labels: {
+                    boxWidth: 12
+                }
+            }
+        }
+    });
 
-    // set the data
-    var data = [
-        {x: "White", value: 223553265},
-        {x: "Black or African American", value: 38929319},
-        {x: "American Indian and Alaska Native", value: 2932248},
-        {x: "Asian", value: 14674252},
-        {x: "Native Hawaiian and Other Pacific Islander", value: 540013},
-        {x: "Some Other Race", value: 19107368},
-        {x: "Two or More Races", value: 9009073}
-    ];
-  
-    // create the chart
-    var chart = anychart.pie();
-  
-    // set the chart title
-    chart.title("Population by Race for the United States: 2010 Census");
-  
-    // add the data
-    chart.data(data);
-  
-    // display the chart in the container
-    chart.container('container');
-    chart.draw();
+    var ctx_2 = document.getElementById("Currently_At").getContext('2d');
+    var data_2 = {
+        datasets: [{
+            data: [10, 20, 30],
+            backgroundColor: [
+                '#3c8dbc',
+                '#f56954',
+                '#f39c12',
+            ],
+        }],
+        labels: [
+            'Request',
+            'Pink',
+            'Problem'
+        ]
+    };
+    var myDoughnutChart_2 = new Chart(ctx_2, {
+        type: 'pie',
+        data: data_2,
+        options: {
+            responsive: false,
+            maintainAspectRatio: false,
+            legend: {
+                position: 'bottom',
+                labels: {
+                    boxWidth: 12
+                }
+            }
+        }
+    });
+    var ctx_2 = document.getElementById("Major").getContext('2d');
+    var data_2 = {
+        datasets: [{
+            data: [10, 20, 30],
+            backgroundColor: [
+                '#3c8dbc',
+                '#f56954',
+                '#f39c12',
+            ],
+        }],
+        labels: [
+            'Request',
+            'Dog',
+            'Problem'
+        ]
+    };
+    var myDoughnutChart_2 = new Chart(ctx_2, {
+        type: 'pie',
+        data: data_2,
+        options: {
+            responsive: false,
+            maintainAspectRatio: false,
+            legend: {
+                position: 'bottom',
+                labels: {
+                    boxWidth: 12
+                }
+            }
+        }
+    });
+});
