f8b98916f760323dd5337bf126ddcabd60f46c68
cantrek2 <78661279+cantrek2@users.noreply.github.com>
2021-12-02 13:51:11 -0800

    Started working on prediction display

---
+++ sketch.js
@@ -0,0 +1,14 @@
+var x = 0;
+function setup() {
+    createCanvas(400, 300);
+  }
+  
+  function draw() {
+    background(220, 0, 200);
+  
+    line(0, 150, 100, 150);
+    rectMode(CENTER);
+
+    //rect(x, 150, 150, 150);
+ 
+  }
